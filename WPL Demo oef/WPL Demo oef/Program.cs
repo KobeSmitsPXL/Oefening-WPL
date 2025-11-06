fdfce48f54c0d21997e0b3c25afc715009413f66
KobeSmitsPXL <Kobe.smits@student.pxl.be>
2025-11-06 13:31:14 +0100

    DEMO-03

--- WPL Demo oef/WPL Demo oef/Program.cs
+++ WPL Demo oef/WPL Demo oef/Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Wat in je gebruikersnaam?");
             string userName = Console.ReadLine();
             Console.WriteLine("Password:");
             string userPassword = Console.ReadLine();
+
+            if (userName == "Papa" && userPassword == "Papaisdebeste2000")
+            {
+                Console.WriteLine("Welkom, admin!");
+            }
+            
+            
         }
     }
 }
