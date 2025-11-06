1ff55538c3d8b569ecda121959aef0c5268d72bd
KobeSmitsPXL <Kobe.smits@student.pxl.be>
2025-11-06 13:24:48 +0100

    DEMO-01

---
+++ WPL Demo oef/WPL Demo oef/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WPL_Demo_oef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wat in je gebruikersnaam?");
+        }
+    }
+}
